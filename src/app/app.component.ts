2e9520ca9ca2465e5cfc321d026d98a39be6b2a9
manhhungpm <hungnm.nuce60@gmail.com>
2018-12-31 00:06:34 +0700

    chia component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  // searchForm = this.fb.group({
+  //   keyword :['']
+  // });
+
+  // constructor(private fb: FormBuilder){}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,45 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import { HeadComponent } from './head/head.component';
+import { FootComponent } from './foot/foot.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const appRoutes: Routes = [
+  {path: 'home' ,component: HomeComponent},
+  {path: 'blog-detail/:id', component: BlogDetailComponent},
+  {path: 'about-me', component:AboutmeComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    AboutmeComponent,
+    BlogDetailComponent,
+    HomeComponent,
+    HeadComponent,
+    FootComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes,{enableTracing: true}),
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+ }
+
+ 
